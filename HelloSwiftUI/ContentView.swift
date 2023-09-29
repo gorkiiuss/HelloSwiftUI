f2c07e262b36796f26cd016b912a3899adea514a
Gorka Puente <gorkapuentediez@gmail.com>
2023-09-29 13:33:59 +0200

    Initial commit

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .imageScale(.medium)
+                .foregroundColor(.orange)
+            Box(title: "Lore Ipsum")
+            Box(title: "Emoji 1", text: "💯")
+            Box(title: "Emoji 2", text: "👊")
+            Box(title: "Emoji 3", text: "🥶")
+            // NavigationLink(destination: SecondView()) {
+            //     Label(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, systemImage: "globe").foregroundColor(.teal)
+            // }
         }
-        .padding()
+    }
+}
+
+struct Box: View {
+    // @State var visible:Bool = true;
+    var title: String
+    var text: String = "";
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 15)
+            VStack {
+                Text(title).foregroundColor(.white)
+                Text(text).foregroundColor(.white).padding()
+            }.onTapGesture {
+                print("Hello, World!")
+                // self.visible.toggle()
+                // print(visible)
+            }
+        }.foregroundColor(.brown).padding(5)
     }
 }
 
